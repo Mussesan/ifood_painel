404c4137cae4856cd20194dd7ea01af0088f6b1a
Gustavo Musse <gmusse.dev@gmail.com>
2022-08-03 17:20:25 -0300

    finalização owl carousel

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $('.melhores-restaurantes').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:3,
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+    
+})
+
+$(document).ready(function(){
+    $('.outdoor-cards').owlCarousel({
+        loop:false,
+        margin:10,
+        responsiveClass:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+    
+})
